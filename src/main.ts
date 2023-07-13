905266d447be228a1ba1b40716d321ed2bf621d4
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-13 17:43:46 +0300

    3

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({credentials: true, origin: true})
+
+  await app.listen(5000);
 }
 bootstrap();
